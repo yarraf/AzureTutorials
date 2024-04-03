b2b6b1514e02464a1614f2a453992db8715a6192
Youness Arraf <youness.arraf@inetum.com>
2024-04-03 11:41:23 +0000

    solution structur +  app console azure.cosmosdb

---
+++ Azure.Tutorials/Azure.cosmodb/Category.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Azure.cosmodb
+{
+    public record Category(
+        string Id,
+        string CategoryId
+    ) : Item(
+        Id,
+        CategoryId,
+        nameof(Category)
+    );
+}
---
+++ Azure.Tutorials/Azure.cosmodb/Product.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace dotnet_cosmos_db_console
+{
+    public record Product(
+      string Id,
+      string CategoryId
+  ) : Item(
+      Id,
+      CategoryId,
+      nameof(Product)
+  )
+    {
+        public string Name { get; init; } = default!;
+        public decimal Price { get; init; }
+        public bool Archived { get; init; }
+        public int Quantity { get; init; }
+    };
+}
