4be32dd15ba329037958921bc2dbf53631b208bf
Youness Arraf <youness.arraf@inetum.com>
2024-04-18 09:23:04 +0100

    create main method add new method to create  comosdb

--- Azure.Tutorials/Azure.cosmodb/Program.cs
+++ Azure.Tutorials/Azure.cosmodb/Program.cs
@@ -7,141 +7,217 @@ using Microsoft.Azure.Documents;
 using System.Configuration;
 using System.Drawing.Text;
 
-const string connectionString = "";
-private const string endPointUri = ConfigurationManager.AppSettings["EndpointUri"];
-private const string primaryKey = ConfigurationManager.AppSettings["PrimaryKey"];
 
-Console.WriteLine($"[Connection string]:\t{connectionString}");
-
-
-CosmosSerializationOptions serializerOptions = new()
+namespace Azure.cosmodb
 {
-    PropertyNamingPolicy = CosmosPropertyNamingPolicy.CamelCase
-};
+    class Program
+    {
 
-using CosmosClient client = new CosmosClientBuilder(connectionString)
-    .WithSerializerOptions(serializerOptions)
-    .Build();
-
-Console.WriteLine("[Client ready]");
-
-
-Console.WriteLine("[starting create database]");
-
-
-Database cosmosdb = await client.CreateDatabaseIfNotExistsAsync("cosmosdbWork");
-
-Console.WriteLine($"[Database created]: \t{cosmosdb.Id}");
+        const string connectionString = "";
+        private static  readonly string endPointUri = ConfigurationManager.AppSettings["EndpointUri"];
+        private static readonly string primaryKey = ConfigurationManager.AppSettings["PrimaryKey"];
+        private CosmosClient cosmosClient;
+        private Database database;
+        private Container container;
+        private string databaseId = "db";
+        private string containerId = "items";
 
 
-///Create container
-ContainerProperties properties = new ContainerProperties(
-                                                            id: "Products",
-                                                            partitionKeyPath: "/categoryId"
-                                                        );
+        public static async Task Main(string[] args)
+        {
+            try
+            {
+                Console.WriteLine("Beginning operations...\n");
+                Program p = new Program();
+                await p.GetStartDemoAsync();
+            }
+            catch (CosmosException e)
+            {
+
+            }
+            catch (Exception ex)
+            {
+
+            }
+            finally
+            {
+                Console.WriteLine("End of demo, press any key to exit");
+                Console.ReadKey();
+            }
 
 
-//autoscaling
-var throughput = ThroughputProperties.CreateAutoscaleThroughput(autoscaleMaxThroughput: 1000);
+        }
 
-Container container = await cosmosdb.CreateContainerIfNotExistsAsync(
-                                                                containerProperties: properties,
-                                                                throughputProperties: throughput
-                                                                );
-Console.WriteLine($"[Container created]: \t{container.Id}");
+        // <GetStartedDemoAsync>
+        /// <summary>
+        /// Entry point to call methods that operate on Azure Cosmos DB resources in this sample
+        /// </summary>
+        private async Task GetStartDemoAsync()
+        {
+            this.cosmosClient = new CosmosClient(EndPointUri,
+                                                PrimaryKey,
+                                                new CosmosClientOptions { ApplicationName = "CosmosDBDotnetQuickstart" }
 
-//create new category instance
+                );
 
-Category goggles = new Category("ef7fa0f1-0e9d-4435-aaaf-a778179a94ad", "gear-snow-goggles");
-
-Microsoft.Azure.Cosmos.PartitionKey goggleskey = new("gear-snow-goggles");
-
-Category result = await container.UpsertItemAsync(goggles, goggleskey);
-
-Console.WriteLine($"[New item created]: \t{result.Id}\t{result.Type}");
-
-//new category
-
-Category helmets = new Category("91f79374-8611-4505-9c28-3bbbf1aa7df7", "gear-climb-helmets");
-
-Microsoft.Azure.Cosmos.PartitionKey helmetskey = new("gear-climb-helmets");
-
-ItemResponse<Category> response = await container.UpsertItemAsync(helmets, helmetskey);
-
-Console.WriteLine($"[New item created]: \t{response.Resource.Id}\t(Type: {response.Resource.Type})\t(RUs: {response.RequestCharge})");
+            await this.CreateDatabaseAsync();
+            //await this.CreateContainerAsync();
+            //await this.ScaleContainerAsync();
+            //await this.AddItemsToContainerAsync();
+            //await this.QueryItemsAsync();
+            //await this.ReplaceFamilyItemAsync();
+            //await this.DeleteFamilyItemAsync();
+            //await this.DeleteDatabaseAndCleanupAsync();
+        }
 
 
-//using transaction to insert in first new category tent related with products
-
-Category tents = new(
-    Id: "5df21ec5-813c-423e-9ee9-1a2aaead0be4",
-    CategoryId: "gear-camp-tents"
-);
-
-Product cirroa = new(
-    Id: "e8dddee4-9f43-4d15-9b08-0d7f36adcac8",
-    CategoryId: "gear-camp-tents"
-)
-{
-    Name = "Cirroa Tent",
-    Price = 490.00m,
-    Archived = false,
-    Quantity = 15
-};
-
-Product kuloar = new(
-    Id: "e6f87b8d-8cd7-4ade-a005-14d3e2fbd1aa",
-    CategoryId: "gear-camp-tents"
-)
-{
-    Name = "Kuloar Tent",
-    Price = 530.00m,
-    Archived = false,
-    Quantity = 8
-};
-
-Product mammatin = new(
-    Id: "f7653468-c4b8-47c9-97ff-451ee55f4fd5",
-    CategoryId: "gear-camp-tents"
-)
-{
-    Name = "Mammatin Tent",
-    Price = 0.00m,
-    Archived = true,
-    Quantity = 0
-};
-
-Product nimbolo = new(
-    Id: "6e3b7275-57d4-4418-914d-14d1baca0979",
-    CategoryId: "gear-camp-tents"
-)
-{
-    Name = "Nimbolo Tent",
-    Price = 330.00m,
-    Archived = false,
-    Quantity = 35
-};
+        // <CreateDatabaseAsync>
+        /// <summary>
+        /// Create the database if it does not exist
+        /// </summary>
+        private async Task CreateDatabaseAsync()
+        {
+            this.database = await this.cosmosClient.CreateDatabaseIfNotExistsAsync(databaseId);
+            Console.WriteLine("Created Database: {0}\n", this.database.Id);
+        }
+        // </CreateDatabaseAsync>
 
 
-Microsoft.Azure.Cosmos.PartitionKey tentsKey = new("gear-camp-tents");
-
-
-TransactionalBatch batch = container.CreateTransactionalBatch(tentsKey)
-    .UpsertItem<Category>(tents)
-    .UpsertItem<Product>(cirroa)
-    .UpsertItem<Product>(kuloar)
-    .UpsertItem<Product>(mammatin)
-    .UpsertItem<Product>(nimbolo);
-
-
-Console.WriteLine("[Batch started]");
-
-using TransactionalBatchResponse batchResponse = await batch.ExecuteAsync();
-
-for (int i = 0; i < batchResponse.Count; i++)
-{
-    TransactionalBatchOperationResult<Item> batchResult = batchResponse.GetOperationResultAtIndex<Item>(i);
-    Console.WriteLine($"[New item created]:\t{batchResult.Resource.Id}\t(Type: {batchResult.Resource.Type})");
+    }
 }
 
-Console.WriteLine($"[Batch completed]:\t(RUs: {batchResponse.RequestCharge})");
+
+//Console.WriteLine($"[Connection string]:\t{connectionString}");
+
+
+//CosmosSerializationOptions serializerOptions = new()
+//{
+//    PropertyNamingPolicy = CosmosPropertyNamingPolicy.CamelCase
+//};
+
+//using CosmosClient client = new CosmosClientBuilder(connectionString)
+//    .WithSerializerOptions(serializerOptions)
+//    .Build();
+
+//Console.WriteLine("[Client ready]");
+
+
+//Console.WriteLine("[starting create database]");
+
+
+//Database cosmosdb = await client.CreateDatabaseIfNotExistsAsync("cosmosdbWork");
+
+//Console.WriteLine($"[Database created]: \t{cosmosdb.Id}");
+
+
+/////Create container
+//ContainerProperties properties = new ContainerProperties(
+//                                                            id: "Products",
+//                                                            partitionKeyPath: "/categoryId"
+//                                                        );
+
+
+////autoscaling
+//var throughput = ThroughputProperties.CreateAutoscaleThroughput(autoscaleMaxThroughput: 1000);
+
+//Container container = await cosmosdb.CreateContainerIfNotExistsAsync(
+//                                                                containerProperties: properties,
+//                                                                throughputProperties: throughput
+//                                                                );
+//Console.WriteLine($"[Container created]: \t{container.Id}");
+
+////create new category instance
+
+//Category goggles = new Category("ef7fa0f1-0e9d-4435-aaaf-a778179a94ad", "gear-snow-goggles");
+
+//Microsoft.Azure.Cosmos.PartitionKey goggleskey = new("gear-snow-goggles");
+
+//Category result = await container.UpsertItemAsync(goggles, goggleskey);
+
+//Console.WriteLine($"[New item created]: \t{result.Id}\t{result.Type}");
+
+////new category
+
+//Category helmets = new Category("91f79374-8611-4505-9c28-3bbbf1aa7df7", "gear-climb-helmets");
+
+//Microsoft.Azure.Cosmos.PartitionKey helmetskey = new("gear-climb-helmets");
+
+//ItemResponse<Category> response = await container.UpsertItemAsync(helmets, helmetskey);
+
+//Console.WriteLine($"[New item created]: \t{response.Resource.Id}\t(Type: {response.Resource.Type})\t(RUs: {response.RequestCharge})");
+
+
+////using transaction to insert in first new category tent related with products
+
+//Category tents = new(
+//    Id: "5df21ec5-813c-423e-9ee9-1a2aaead0be4",
+//    CategoryId: "gear-camp-tents"
+//);
+
+//Product cirroa = new(
+//    Id: "e8dddee4-9f43-4d15-9b08-0d7f36adcac8",
+//    CategoryId: "gear-camp-tents"
+//)
+//{
+//    Name = "Cirroa Tent",
+//    Price = 490.00m,
+//    Archived = false,
+//    Quantity = 15
+//};
+
+//Product kuloar = new(
+//    Id: "e6f87b8d-8cd7-4ade-a005-14d3e2fbd1aa",
+//    CategoryId: "gear-camp-tents"
+//)
+//{
+//    Name = "Kuloar Tent",
+//    Price = 530.00m,
+//    Archived = false,
+//    Quantity = 8
+//};
+
+//Product mammatin = new(
+//    Id: "f7653468-c4b8-47c9-97ff-451ee55f4fd5",
+//    CategoryId: "gear-camp-tents"
+//)
+//{
+//    Name = "Mammatin Tent",
+//    Price = 0.00m,
+//    Archived = true,
+//    Quantity = 0
+//};
+
+//Product nimbolo = new(
+//    Id: "6e3b7275-57d4-4418-914d-14d1baca0979",
+//    CategoryId: "gear-camp-tents"
+//)
+//{
+//    Name = "Nimbolo Tent",
+//    Price = 330.00m,
+//    Archived = false,
+//    Quantity = 35
+//};
+
+
+//Microsoft.Azure.Cosmos.PartitionKey tentsKey = new("gear-camp-tents");
+
+
+//TransactionalBatch batch = container.CreateTransactionalBatch(tentsKey)
+//    .UpsertItem<Category>(tents)
+//    .UpsertItem<Product>(cirroa)
+//    .UpsertItem<Product>(kuloar)
+//    .UpsertItem<Product>(mammatin)
+//    .UpsertItem<Product>(nimbolo);
+
+
+//Console.WriteLine("[Batch started]");
+
+//using TransactionalBatchResponse batchResponse = await batch.ExecuteAsync();
+
+//for (int i = 0; i < batchResponse.Count; i++)
+//{
+//    TransactionalBatchOperationResult<Item> batchResult = batchResponse.GetOperationResultAtIndex<Item>(i);
+//    Console.WriteLine($"[New item created]:\t{batchResult.Resource.Id}\t(Type: {batchResult.Resource.Type})");
+//}
+
+//Console.WriteLine($"[Batch completed]:\t(RUs: {batchResponse.RequestCharge})");
